66c7f77d835dd29563db8c6274dbac90bbab40b9
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    sm8250-common: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -231,4 +231,4 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
--include vendor/sony/sm8250-common/BoardConfigVendor.mk
+include vendor/sony/sm8250-common/BoardConfigVendor.mk
