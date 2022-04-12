12f78167e0de875fe521ce9c4109ef9f91f38b50
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    sm8250-common: Pick up USB configurations from QCOM
     * If we want to override anything in particular, e.g.
       PID/VIDs we will create a seperate script named
       'init.sony.rc'.
    
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -97,14 +97,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.usb.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/init.qcom.usb.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qti.chg_policy.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -149,14 +141,6 @@ LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.usb.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
@@ -241,14 +225,6 @@ LOCAL_MODULE           := init.sony-platform.rc
 LOCAL_MODULE_TAGS      := optional
 LOCAL_MODULE_CLASS     := ETC
 LOCAL_SRC_FILES        := etc/init.sony-platform.rc
-LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE           := init.sony.usb.rc
-LOCAL_MODULE_TAGS      := optional
-LOCAL_MODULE_CLASS     := ETC
-LOCAL_SRC_FILES        := etc/init.sony.usb.rc
 LOCAL_MODULE_PATH      := $(TARGET_OUT_VENDOR_ETC)/init
 include $(BUILD_PREBUILT)
 
