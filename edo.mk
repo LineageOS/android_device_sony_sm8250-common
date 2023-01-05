65ca94c1d75c2066db21335420b9a203be1046c4
Luofan Chen <clfbbn@gmail.com>
2023-01-06 00:44:26 +0800

    sm8250-common: overlay: Import CarrierConfig from 58.2.A.7.93
    Change-Id: I61445871939d133afca76223d8b3763c67e8b37e

--- edo.mk
+++ edo.mk
@@ -34,6 +34,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_PACKAGES += \
+    CarrierConfigResCommon
 
 # Permissions
 PRODUCT_COPY_FILES += \
