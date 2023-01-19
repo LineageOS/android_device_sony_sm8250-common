0d3d0b9fb44699f496bce95968c8df064d45cc33
Luofan Chen <clfbbn@gmail.com>
2023-01-19 19:22:54 +0800

    sm8250-common: overlay: Convert to RRO packages
    Change-Id: I6f0af19c7e2d0ae0ee67c54ac20399c6bd719443

--- edo.mk
+++ edo.mk
@@ -30,12 +30,17 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    CarrierConfigResCommon
+    CarrierConfigResCommon \
+    SonyEdoFrameworksResCommon \
+    SonyEdoSettingsProviderOverlayCommon \
+    SonyEdoSettingsResCommon \
+    SonyEdoSystemUIResCommon \
+    SonyEdoTelephonyResCommon \
+    WifiResCommon
 
 # Permissions
 PRODUCT_COPY_FILES += \
