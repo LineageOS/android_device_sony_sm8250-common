95347451264c5bf112c24c5c2b69915342a34786
kyasu <kyasu.dev@gmail.com>
2023-01-22 16:21:20 +0900

    sm8250-common: init.qcom.msim.sh: Find the exact model
    Change-Id: I42b1dca6e8d95bdb018460b32e186332d8c309ad

--- rootdir/bin/init.qcom.msim.sh
+++ rootdir/bin/init.qcom.msim.sh
@@ -1,20 +1,10 @@
 #!/vendor/bin/sh
 
-MSIM_DEVICES=(
-    xq-at42 xq-at52 xq-at72 # Xperia 1 II Dual-SIM version
-    xq-as42 xq-as52 xq-as62 xq-as72 # Xperia 5 II Dual-SIM version
-)
-MSIM_DEVICE=0
+model=`sed -e '/[Mm][Oo][Dd][Ee][Ll]: /!d' -e 's/^.*[Mm][Oo][Dd][Ee][Ll]: \([A-Za-z0-9-]*\).*$/\1/' -e '/^$/d' /dev/block/bootdevice/by-name/LTALabel` 2> /dev/null
 
-for device in "${MSIM_DEVICES[@]}"; do
-    if grep -qi "Model: ${device}" /dev/block/bootdevice/by-name/LTALabel; then
-        MSIM_DEVICE=1
-        break
-    fi
-done
-
-if [[ "${MSIM_DEVICE}" -eq 1 ]]; then
-    setprop vendor.radio.multisim.config dsds
-else
-    setprop vendor.radio.multisim.config ss
-fi
+case "$model" in
+    "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
+        setprop vendor.radio.multisim.config dsds;;
+    * )
+        setprop vendor.radio.multisim.config ss;;
+esac
