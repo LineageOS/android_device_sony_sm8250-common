37716b66384c9c8c4b98fb6b5c1202fce6185271
kyasu <kyasu.dev@gmail.com>
2023-01-22 16:21:41 +0900

    sm8250-common: Add FeliCa (NFC Type-F) support for JP model
    Change-Id: Ic2e8c21b10af814be1d54ec944eca38c90974aea

--- rootdir/bin/init.qcom.msim.sh
+++ rootdir/bin/init.qcom.msim.sh
@@ -3,8 +3,12 @@
 model=`sed -e '/Model: /!d' -e 's/^.*Model: \([A-Za-z0-9-]*\).*$/\1/' /dev/block/bootdevice/by-name/LTALabel` 2> /dev/null
 
 case "$model" in
-    "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
+    "XQ-AT52" | "XQ-AT72" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
         setprop vendor.radio.multisim.config dsds;;
+    "XQ-AS42" | "XQ-AT42" )
+        setprop vendor.radio.multisim.config dsds
+        setprop persist.vendor.nfc.config_file_name "libnfc-nxp-typef.conf"
+        ;;
     * )
         setprop vendor.radio.multisim.config ss;;
 esac
