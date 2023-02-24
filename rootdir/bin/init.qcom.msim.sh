64c96017f95870051beaab6ca7f72c7f71fc9c10
kyasu <kyasu.dev@gmail.com>
2023-02-24 19:07:00 +0900

    fixup! sm8250-common: init.qcom.msim.sh: Find the exact model
    * LTALabel of XQ-AT42 has two spaces after 'MODEL:'.
    * LTALabel of XQ-AT52 has two lines containing 'MODEL:'.
    
    Change-Id: Ifa4ec37853314345a531973e3d1fbbcd41195392

--- rootdir/bin/init.qcom.msim.sh
+++ rootdir/bin/init.qcom.msim.sh
@@ -1,6 +1,6 @@
 #!/vendor/bin/sh
 
-model=`sed -e '/[Mm][Oo][Dd][Ee][Ll]: /!d' -e 's/^.*[Mm][Oo][Dd][Ee][Ll]: \([A-Za-z0-9-]*\).*$/\1/' -e '/^$/d' /dev/block/bootdevice/by-name/LTALabel` 2> /dev/null
+model=`grep -am1 '[Mm][Oo][Dd][Ee][Ll]:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*[Mm][Oo][Dd][Ee][Ll]:[ ]*\([A-Za-z0-9-]*\).*$/\1/'` 2> /dev/null
 
 case "$model" in
     "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
