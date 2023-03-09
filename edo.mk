294223dea8183b7ee527b96a32987890c5d78193
Georg Veichtlbauer <georg@vware.at>
2023-03-09 15:34:20 +0100

    sm8250-common: Remove unneeded NFC packages
    * NfcNci and SecureElement are built by handheld_system.mk
    
    Change-Id: I83f686b67778634b584a58d8bae95d237e6dbad1

--- edo.mk
+++ edo.mk
@@ -387,7 +387,6 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.1.vendor \
     android.hardware.secure_element@1.2.vendor \
     android.hardware.nfc@1.2.vendor \
-    NfcNci \
     SecureElement \
     Tag
 
