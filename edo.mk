7b182778f117b2322a42713276630a175dcaf5ca
Luofan Chen <clfbbn@gmail.com>
2023-11-03 03:34:55 +0800

    sm8250-common: Use libbinder and libhidlbase from vndk 32 for camera HAL
    Change-Id: Icff1ca545f7d179fa7f4a88597936196266436e8

--- edo.mk
+++ edo.mk
@@ -26,7 +26,9 @@ PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib/libutils-v32.so \
-    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-core/libbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinder-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
