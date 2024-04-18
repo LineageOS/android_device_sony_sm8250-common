11b3feedbbc799739a201cd44b9636622f850761
kyasu <kyasu.dev@gmail.com>
2024-04-18 21:50:09 +0900

    sm8250-common: Drop QCOM WFD
    Use AOSP WFD.
    
    Change-Id: I429b9974db3c4c73408ec1501f6c6359f39b4c62

--- edo.mk
+++ edo.mk
@@ -537,17 +537,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# WiFi Display
-PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
-
 # XperiaParts
 PRODUCT_PACKAGES += \
     XperiaParts
