LineageOS/android_device_oneplus_sm8250-common

sm8250-common: Enforce vendor tree presence
A compiled build has no chance of booting without the prebuilt
binaries, so fail the build if those are not present.

Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e

BoardConfigCommon.mk
@@ -221,4 +221,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/oneplus/sm8250-common/BoardConfigVendor.mk
+include vendor/oneplus/sm8250-common/BoardConfigVendor.mk

LineageOS/android_device_sony_sm8250-common

Signed-off-by: SirRGB <sirrgb@proton.me>

BoardConfigCommon.mk
@@ -231,4 +231,4 @@ WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
--include vendor/sony/sm8250-common/BoardConfigVendor.mk
+include vendor/sony/sm8250-common/BoardConfigVendor.mk