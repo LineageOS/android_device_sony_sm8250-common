d30ed2363b4f7959da129099b23bbe5704514523
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-25 00:19:16 +0800

    sm8250-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710

--- edo.mk
+++ edo.mk
@@ -369,7 +369,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
