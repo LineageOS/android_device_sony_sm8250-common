560621128a8be3cb7edf5330fb4dfbcaa533834d
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    sm8250-common: Remove OMX audio codecs
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
    
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f

--- edo.mk
+++ edo.mk
@@ -309,12 +309,7 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
