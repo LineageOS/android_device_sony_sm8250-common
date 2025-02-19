3f1efbfe104690a98e7f8d0b8b6206fa7e972a88
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sm8250-common: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I04ab771d3b1c38b58913607fbff1bb3b55e1fe25

--- edo.mk
+++ edo.mk
@@ -191,6 +191,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Boot control
+PRODUCT_SOONG_NAMESPACES += hardware/qcom-caf/bootctrl
+
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
     android.hardware.boot-service.qti.recovery
