47c4cc34f0fa8172ac3880ab73620862da79ed0b
kyasu <kyasu.dev@gmail.com>
2025-03-25 11:48:55 +0900

    sm8250-common: Add SKU manifests for single-SIM and dual-SIM devices
    AOSP Radio HIDL->AIDL Wrapper requires different manifests for
    single-SIM and dual-SIM devices.
    
    Change-Id: Ifcbc1c81da92bdbd9b7fe097c98e9ffc9532b0f1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -138,6 +138,10 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
+ODM_MANIFEST_SKUS := ss ds
+ODM_MANIFEST_SS_FILES := $(COMMON_PATH)/manifest_ss.xml
+ODM_MANIFEST_DS_FILES := $(COMMON_PATH)/manifest_ds.xml
+
 # Media
 TARGET_USES_ION := true
 
