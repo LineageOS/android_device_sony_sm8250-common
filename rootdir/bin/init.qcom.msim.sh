69a98577c2a673782c51f7e1dffc8597d38dae8e
kyasu <kyasu.dev@gmail.com>
2025-03-27 09:08:51 +0900

    sm8250-common: Set ro.boot.product.hardware.sku during early-init
    Change-Id: Ic63e456436075128a9019dce9760795ca1c1fe74

--- rootdir/bin/init.qcom.msim.sh
+++ rootdir/bin/init.qcom.msim.sh
@@ -1,12 +1,12 @@
 #!/vendor/bin/sh
 
-model=`grep -aim1 'model:' /dev/block/bootdevice/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
+model=`grep -aim1 'model:' /dev/block/by-name/LTALabel | sed -e 's/^.*model:[ ]*\([A-Za-z0-9-]*\).*$/\1/I'` 2> /dev/null
 
 case "$model" in
     "XQ-AT42" | "XQ-AT52" | "XQ-AT72" | "XQ-AS42" | "XQ-AS52" | "XQ-AS62" | "XQ-AS72" )
-        setprop vendor.radio.multisim.config dsds;;
+        setprop vendor.radio.hardware.sku ds;;
     * )
-        setprop vendor.radio.multisim.config ss;;
+        setprop vendor.radio.hardware.sku ss;;
 esac
 
 if [ "$model" == "" ]; then
