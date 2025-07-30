6b6c58c3eab6cfda01feb1a5708169cb321e7f31
kyasu <kyasu.dev@gmail.com>
2025-07-30 23:06:15 +0900

    sm8250-common: Disable `thumbnail_block_model` of libstagefright
    Change-Id: I86f92335af282d3de508150bbbcb3026993ec48c

--- edo.mk
+++ edo.mk
@@ -290,6 +290,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_ODM)/etc/media_profiles_V1_0.xml \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
+$(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
+
 # Light
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.sony_edo
