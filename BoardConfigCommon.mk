ba53ed06f7f8040b3788da98d09a2b1a8e80b1b1
kyasu <kyasu.dev@gmail.com>
2025-07-31 17:20:49 +0900

    sm8250-common: Change to use kernel config fragments
    Change-Id: I209be981b27c05528308bdaeeed7ec76de7d5040

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,6 +60,9 @@ BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_DTB_OFFSET           := 0x01f00000
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DTBO := true
+TARGET_KERNEL_CONFIG := \
+    vendor/kona-perf_defconfig \
+    vendor/edo.config
 
 # Use External DTC
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
