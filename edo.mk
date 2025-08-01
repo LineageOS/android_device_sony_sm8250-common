0dadec5065bde0e4160b032b0221b5900925d919
CLF BBN <clfbbn@gmail.com>
2025-08-01 11:44:03 +0000

    Revert "sm8250-common: Migrate to QTI USB Gadget AIDL HAL"
    Revert submission 436228
    
    Reason for revert: Re-apply after I7d4943802ab6fb6af41dd083f9bced6b740aa0f1 is merged
    
    Reverted changes: /q/submissionid:436228
    
    Change-Id: Id6456cf2e718fc98563eac08b2ddea7bf10a6080

--- edo.mk
+++ edo.mk
@@ -369,8 +369,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti
+    android.hardware.usb-service.qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
