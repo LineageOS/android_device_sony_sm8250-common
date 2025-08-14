27e67f161cfec19929fd255b78f447ee661a2473
LuK1337 <priv.luk@gmail.com>
2025-08-14 23:47:18 +0200

    sm8250-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- edo.mk
+++ edo.mk
@@ -293,7 +293,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.sony
+    vendor.lineage.livedisplay-service.sony
 
 # NFC
 PRODUCT_COPY_FILES += \
