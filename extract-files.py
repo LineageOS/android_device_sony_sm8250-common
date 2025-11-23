3ecf4fec148cb713d6f40d87dd9c32951ce659b9
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    sm8250-common: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -111,6 +111,10 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libsomc_chokoballcmn.so',
     ): blob_fixup()
         .replace_needed('libui.so', 'libui-v34.so'),
+    (
+        'system_ext/lib64/libwfdnative.so',
+    ) : blob_fixup()
+        .add_needed('libinput_shim.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
