42120d9dab5091bc64af9abcbaf3bb07e8b46aee
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sm8250-common: Patch some blobs to depend on libtinyxml2-v34.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -115,6 +115,10 @@ blob_fixups: blob_fixups_user_type = {
         'system_ext/lib64/libwfdnative.so',
     ) : blob_fixup()
         .add_needed('libinput_shim.so'),
+    (
+        'vendor/lib64/libdpps.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
