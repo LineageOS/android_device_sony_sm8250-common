562bd5fcaeadd535a4f2fa838c0187c1effcc75d
kyasu <kyasu.dev@gmail.com>
2026-01-04 10:16:29 +0900

    Revert "sm8250-common: Remove OMX audio codecs"
    This reverts commit 560621128a8be3cb7edf5330fb4dfbcaa533834d.
    
    Change-Id: Ibf3683e9731883b26fb9b37571e53c7f5341be50

--- edo.mk
+++ edo.mk
@@ -314,7 +314,12 @@ PRODUCT_PACKAGES += \
 
 # OMX
 PRODUCT_PACKAGES += \
+    libOmxAacEnc \
+    libOmxAmrEnc \
     libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
