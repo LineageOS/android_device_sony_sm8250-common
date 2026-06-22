018b91c8e28c3bf45ba5a0fff44201d43791452d
Michael Bestas <mkbestas@lineageos.org>
2026-06-22 03:46:06 +0300

    sm8250-common: Update WFD system blobs from marvel_g A171WEH.20
    Change-Id: I733c2bf342175474ce7f02d467a17ded6f0a8760

--- extract-files.py
+++ extract-files.py
@@ -63,6 +63,10 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system/framework/WfdCommon.jar': blob_fixup()
+        .apktool_patch('blob-patches/WfdCommon.patch'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     (
         'vendor/lib64/libvpplibrary.so',
         'vendor/lib64/libswiqisettinghelper.so',
@@ -111,10 +115,6 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/libsomc_chokoballcmn.so',
     ): blob_fixup()
         .replace_needed('libui.so', 'libui-v34.so'),
-    (
-        'system_ext/lib64/libwfdnative.so',
-    ) : blob_fixup()
-        .add_needed('libinput_shim.so'),
     (
         'vendor/lib64/libdpps.so',
     ): blob_fixup()
